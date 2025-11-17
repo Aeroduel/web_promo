3b6ddc3a070230bfd504760fa9f3dd99b96d1f5c
Zytronium <dastelljes@outlook.com>
2025-11-17 17:56:24 -0600

    Add back the O in "AERODUEL"

--- src/app/compontents/HeroSection.tsx
+++ src/app/compontents/HeroSection.tsx
@@ -8,7 +8,7 @@ export function HeroSection() {
         className="fixed top-0 left-0 w-screen h-screen bg-gradient-to-br from-darkernavy via-darkernavy/20 to-navy"></div>
       <div className="z-1 px-4 py-2 flex flex-col items-center">
         <h1 className="text-accent text-shadow-lg title flex items-center tracking-wider font-thin">
-          AER
+          AERO
           <Image src={Logo} alt="O" className="inline-block h-[1.6em] w-auto mb-3" />
           DUEL
         </h1>
