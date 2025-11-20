7e36b025e6adbf174a8a4626ce7058c76f5c36d3
Zytronium <dastelljes@outlook.com>
2025-11-19 18:26:04 -0600

    Swap main logo banner with another variation

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import Logo from "../../../public/two-jets-logo.svg"
+import Logo from "../../../public/aero-duel-wide-banner.svg"
 import Image from "next/image";
 
 export function HeroSection() {
@@ -8,8 +8,8 @@ export function HeroSection() {
         className="fixed top-0 left-0 w-screen h-screen bg-gradient-to-br from-darkernavy via-darkernavy/20 to-navy"></div>
       <div className="z-1 px-4 py-2 flex flex-col items-center">
         <Image src={Logo} width={1000} alt="Aeroduel Logo"/>
-        <p className="subtitle text-gold">
-          The future of RC aerial combat. Real FPV immersion. Real engineering.
+        <p className="subtitle text-gold font-bold"> {/* Hey that rhymes! */}
+          The future of RC aerial combat. Real dogfights. Real FPV immersion. Real engineering.
         </p>
       </div>
     </section>
